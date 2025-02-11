a903f12733fd2c2d40aeb2e53e08e6ae5e2ebb44
stevopablo <stevaopablo@gmail.com>
2025-02-11 13:59:45 -0300

    main and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,13 @@ import logoSpotify from '../assets/spotify-logo.png'
 function Header() {
   return (
     <div className='header'>
+
         <img src={logoSpotify} alt="logoSpotify" />
 
         <a href="/">
         <h1>Spotify</h1>
         </a>
+
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 function Main() {
   return (
     <div className='main'>
+        <div className='main__texts'>
+
         <h2>Artistas mais tocados</h2>
         
         <a href="/">
         <p>Mostrar mais</p>
         </a>
+        </div>
     </div>
   )
 }
