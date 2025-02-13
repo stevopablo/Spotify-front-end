5def79d657e614e1624013f030e7c3b7a406a9eb
pablo <stevaopabllo@gmail.com>
2025-02-13 19:43:18 -0300

    gotta find this bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import logoSpotify from '../assets/spotify-logo.png'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <div className='header'>
-        
+        <Link to={'/'}>
         <img src={logoSpotify} alt="logoSpotify" />
+        </Link>
 
         <a className='header__link' href="/">
         <h1>Spotify</h1>
